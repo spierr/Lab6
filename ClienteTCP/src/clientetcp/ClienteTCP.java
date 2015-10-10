407b9cc0a0c00238dd2a9be14d70d3453f7bc64a
DELL <DELL@LATITUDE-E7240>
2015-10-10 10:13:56 -0500

    Inicial commit

--- ClienteTCP/src/clientetcp/ClienteTCP.java
+++ ClienteTCP/src/clientetcp/ClienteTCP.java
@@ -5,6 +5,13 @@
  */
 package clientetcp;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.UnknownHostException;
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
+
 /**
  *
  * @author DELL
@@ -15,7 +22,40 @@ public class ClienteTCP {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+         try
+        {
+        //Mo 1 client socket den server voi so cong va dia chi xac dinh
+        SSLSocketFactory factory=(SSLSocketFactory) SSLSocketFactory.getDefault();
+        SSLSocket sslsocket=(SSLSocket) factory.createSocket("127.0.0.1",1234);
+
+        //Tao luong nhan va gui du lieu len server
+        DataOutputStream os=new DataOutputStream(sslsocket.getOutputStream());
+        DataInputStream is=new DataInputStream(sslsocket.getInputStream());
+
+        //Gui du lieu len server
+        String str="helloworld";
+        os.writeBytes(str);
+
+        //Nhan du lieu da qua xu li tu server ve
+        String responseStr;
+        if((responseStr=is.readUTF())!=null)
+        {
+            System.out.println(responseStr);
+        }
+
+        os.close();
+        is.close();
+        sslsocket.close();
+        }
+        catch(UnknownHostException e)
+        {
+             System.out.println(e.getMessage());
+        }
+        catch(IOException e)
+        {
+            System.out.println(e.getMessage());
+        }
+
     }
     
 }
--- Servidor/src/servidor/Servidor.java
+++ Servidor/src/servidor/Servidor.java
@@ -5,6 +5,13 @@
  */
 package servidor;
 
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import javax.net.ssl.SSLServerSocket;
+import javax.net.ssl.SSLServerSocketFactory;
+import javax.net.ssl.SSLSocket;
+
 /**
  *
  * @author DELL
@@ -15,7 +22,29 @@ public class Servidor {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       try{
+        //Creaet a SSLServersocket
+        SSLServerSocketFactory factory=(SSLServerSocketFactory) SSLServerSocketFactory.getDefault();
+        SSLServerSocket sslserversocket=(SSLServerSocket) factory.createServerSocket(1234);
+        //Tạo 1 đối tượng Socket từ serversocket để lắng nghe và chấp nhận kết nối từ client
+        SSLSocket sslsocket=(SSLSocket) sslserversocket.accept();
+        //Tao cac luong de nhan va gui du lieu cua client
+        DataInputStream is=new DataInputStream(sslsocket.getInputStream());
+        PrintStream os=new PrintStream(sslsocket.getOutputStream());
+        while(true)  //khi dang ket noi voi client
+        {
+            //Doc du lieu den
+            String input=is.readUTF();
+            String ketqua=input.toUpperCase();
+            //Du lieu tra ve
+            os.println(ketqua);
+        }
+        }
+        catch(IOException e)
+        {
+           System.out.print(e);
+        }
+
     }
     
 }
